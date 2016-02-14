bbf0b8780aa287d8f421fb706ae302d081ec2bc2
Olivier Laviale <olivier.laviale@gmail.com>
2016-02-15 00:10:36 +0100

    Brickrouge v3

--- public/widget.js
+++ public/widget.js
@@ -1,22 +1,31 @@
+!function (Brickrouge) {
 
-Brickrouge.Widget.SearchCombo = new Class ({
+	Brickrouge.Widget.SearchCombo = new Class ({
 
-	initialize: function(el, options)
-	{
-		this.element = $(el);
-		this.element.getChildren().addEvents ({
+		initialize: function(el, options)
+		{
+			this.element = el
+			this.element.getChildren().addEvents ({
 
-			focus: function()
-			{
-				el.addClass('focus');
-			},
+				focus: function()
+				{
+					el.classList.add('focus')
+				},
 
-			blur: function()
-			{
-				el.removeClass('focus');
-			}
+				blur: function()
+				{
+					el.classList.remove('focus')
+				}
 
-		});
-	}
+			});
+		}
 
-});
+	})
+
+	Brickrouge.register('SearchCombo', function (element, options) {
+
+		return new Brickrouge.Widget.SearchCombo(element, options)
+
+	})
+
+} (Brickrouge);
